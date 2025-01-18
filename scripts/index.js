0d7fdb808ccd79b2a8d30d2e4e1529dad9df3efe
Yu Assai <julyshb@yandex.ru>
2025-01-18 19:24:39 +0300

    Теперь точно первая проверка

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,21 @@
 // находим в DOM место для карточек и темплейт карточки
-const cardContainer = document.querySelector('.places__list');
-const cardTemplate = document.querySelector('#card-template').content;
-
+const cardTemplate = document.querySelector("#card-template").content;
+const cardContainer = document.querySelector(".places__list");
 //клонируем шаблон карточки, добавляем в нее изображение и описание. определяем кнопку удаления карточки, устанавливаем слушатель на кнопку.
-function addCard((initialCards.name, initialCards.link), cardDelete) {
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-  cardElement.querySelector('.card__image').src = initialCards.link;
-  cardElement.querySelector('.card__title').textContent = initialCards.name;
-
-  const cardDelete = cardContainer.querySelector('.card__delete-button');
-  cardDelete.addEventListener('click', cardDelete);
-  cardContainer.append(cardElement);
-}
-addCard();
-
-//удаляем карточку
-deleteButtonActive('click', cardDelete {
-  cardDelete.closest('.card');
-  cardDelete.remove();
-});
-
-//перебираем массив с карточками и загружаем карточки на страницу
-function add() {
-  initialCards.forEach(card) {
-    cardContainer.append(addCard(addCard, deleteButtonActive));
-  };
+function createCard(cardData, deleteCard) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", deleteCard);
+  return cardElement;
 };
+//удаляем карточку
+function handleDeleteCard(evt){
+  evt.target.closest(".card").remove();
+};
+//перебираем массив с карточками и загружаем карточки на страницу
+initialCards.forEach((dataCard) => {
+  const cardElement = createCard(dataCard, handleDeleteCard);
+  cardContainer.append(cardElement);
+});
