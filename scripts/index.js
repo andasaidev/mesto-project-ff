9ed7822ef83ee0e5946396e6556a48112100b585
Yu Assai <julyshb@yandex.ru>
2025-01-18 14:35:14 +0300

    first view

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+// находим в DOM место для карточек и темплейт карточки
+const cardContainer = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+//клонируем шаблон карточки, добавляем в нее изображение и описание. определяем кнопку удаления карточки, устанавливаем слушатель на кнопку.
+function addCard((initialCards.name, initialCards.link), cardDelete) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = initialCards.link;
+  cardElement.querySelector('.card__title').textContent = initialCards.name;
 
-// @todo: Функция создания карточки
+  const cardDelete = cardContainer.querySelector('.card__delete-button');
+  cardDelete.addEventListener('click', cardDelete);
+  cardContainer.append(cardElement);
+}
+addCard();
 
-// @todo: Функция удаления карточки
+//удаляем карточку
+deleteButtonActive('click', cardDelete {
+  cardDelete.closest('.card');
+  cardDelete.remove();
+});
 
-// @todo: Вывести карточки на страницу
+//перебираем массив с карточками и загружаем карточки на страницу
+function add() {
+  initialCards.forEach(card) {
+    cardContainer.append(addCard(addCard, deleteButtonActive));
+  };
+};
